a2a82c0a1cd5ff53aab50064b88a0160ae944c6f
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2017-03-02 18:53:20 -0300

    imprimindo a tabela no formato canonico

---
+++ simplex_tabela.py
@@ -0,0 +1,26 @@
+
+print "#" * 20
+print "Iniciando o programa"
+print "#" * 20
+print ""
+
+def simplex(tabela):
+    mostrar_tabela(tabela)
+    
+
+def mostrar_tabela(tabela):
+    for i in range((len(tabela))):
+        for j in range(len(tabela[0])):
+            print tabela[i][j],'\t',
+        print ''
+
+
+
+#Forma canonica
+tabela_base = [['Z', -3, -2, -5, 0, 0, 0, 0],
+               ['X4', 1, 2, 3, 1, 0, 0, 430],
+               ['X5', 3, 0, 2, 0, 1, 0, 460],
+               ['X6', 1, 4, 0, 0, 0, 1, 420]]
+
+
+simplex(tabela_base)
