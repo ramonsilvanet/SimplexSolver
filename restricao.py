f4633c4ad72037f042dd12a936c40e78ff32bb97
Ramon Ferreira Silva <rsilva@ramonsilva.net>
2017-03-05 14:30:21 -0300

    Simplex Tableau

--- restricao.py
+++
@@ -1,5 +0,0 @@
-import theano
-from theano import tensor
-
-ppl = [2,5]
-restricoes = [[],[],[]]
--- simplex_tabela.py
+++ simplex_tabela.py
@@ -1,31 +1,86 @@
+from __future__ import division
+from numpy import *
 
-print "#" * 20
-print "Iniciando o programa"
-print "#" * 20
-print ""
+class Tableau:
 
-def simplex(tabela):
-    mostrar_tabela(tabela)
+    def __init__(self, ppl):
+        self.ppl = [1] + ppl
+        self.rows = []
+        self.cons = []
 
-def simplex_fase_i(tabela):
-    print "fase I"
+    def adic_restr(self, expression, value):
+        self.rows.append([0] + expression)
+        self.cons.append(value)
 
-def simplex_fase_ii(tabela):
-    print "fase II"
+    def _pivot_coluna(self):
+        low = 0
+        idx = 0
+        for i in range(1, len(self.ppl)-1):
+            if self.ppl[i] < low:
+                low = self.ppl[i]
+                idx = i
+        if idx == 0: return -1
+        return idx
 
-def mostrar_tabela(tabela):
-    for i in range((len(tabela))):
-        for j in range(len(tabela[0])):
-            print tabela[i][j],'\t',
-        print ''
+    def _pivot_linha(self, col):
+        rhs = [self.rows[i][-1] for i in range(len(self.rows))]
+        lhs = [self.rows[i][col] for i in range(len(self.rows))]
+        ratio = []
+        for i in range(len(rhs)):
+            if lhs[i] == 0:
+                ratio.append(99999999 * abs(max(rhs)))
+                continue
+            ratio.append(rhs[i]/lhs[i])
+        return argmin(ratio)
 
+    def exibir_tabela(self):
+        print '\n', matrix([self.ppl] + self.rows)
 
+    def _pivot(self, row, col):
+        e = self.rows[row][col]
+        self.rows[row] /= e
+        for r in range(len(self.rows)):
+            if r == row: continue
+            self.rows[r] = self.rows[r] - self.rows[r][col]*self.rows[row]
+        self.ppl = self.ppl - self.ppl[col] * self.rows[row]
 
-#Forma canonica
-tabela_base = [['Z', -3, -2, -5, 0, 0, 0, 0],
-               ['X4', 1, 2, 3, 1, 0, 0, 430],
-               ['X5', 3, 0, 2, 0, 1, 0, 460],
-               ['X6', 1, 4, 0, 0, 0, 1, 420]]
+    def _verificar_solucao(self):
+        if min(self.ppl[1:-1]) >= 0: return 1
+        return 0
 
+    def resolver(self):
 
-simplex(tabela_base)
+        # build full tableau
+        for i in range(len(self.rows)):
+            self.ppl += [0]
+            ident = [0 for r in range(len(self.rows))]
+            ident[i] = 1
+            self.rows[i] += ident + [self.cons[i]]
+            self.rows[i] = array(self.rows[i], dtype=float)
+        self.ppl = array(self.ppl + [0], dtype=float)
+
+        # solve
+        self.exibir_tabela()
+        while not self._verificar_solucao():
+            c = self._pivot_coluna()
+            r = self._pivot_linha(c)
+            self._pivot(r,c)
+            print '\npivot column: %s\npivot row: %s'%(c+1,r+2)
+            self.exibir_tabela()
+
+if __name__ == '__main__':
+
+    """
+    max z = 2x + 3y + 2z
+    st
+    2x + y + z <= 4
+    x + 2y + z <= 7
+    z          <= 5
+    x,y,z >= 0
+    """
+
+    t = Tableau([-2,-3,-2])
+    t.adic_restr([2, 1, 1], 4)
+    t.adic_restr([1, 2, 1], 7)
+    t.adic_restr([0, 0, 1], 5)
+    t.resolver()
